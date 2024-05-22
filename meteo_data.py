ad37c3a5c8f20600c7aeb71ecdd9948cf5fc5e4d
scriubuguri <scriubuguri@tragdate.ninja>
2024-05-22 18:28:33 +0000

    refactoring & OOP implementation

--- meteo_data.py
+++ meteo_data.py
@@ -1,88 +1,62 @@
 #!/usr/bin/env python
-#Weather data and forecast for your city
-#Author: scriubuguri
+# Weather data and forecast for your city
+# Author: scriubuguri
 
-import json
 import requests
 import argparse
 import os
 
 
-#function to get weather data
-def get_weather(param):
-    if 'weather_data_key' in os.environ:
-        API_KEY = os.environ['weather_data_key']
-    else:
-        API_KEY = "your_api_key"
+class Weather:
+    def __init__(self, city, system="metric", verbose=False):
+        self.city = city
+        self.system = system
+        self.verbose = verbose
+        self.api_key = os.environ.get('weather_data_key', 'your_api_key')
+        self.base_url = "https://api.openweathermap.org/data/2.5/weather"
+        self.system_options = {"metric": "C", "imperial": "F", "standard": "K"}
+        self.weather_data = self.get_weather()
 
-    FULL_URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={system}".format(
-        city=param['city'],
-        api_key=API_KEY,
-        system=param['system']
-    )
+    def get_weather(self):
+        params = {
+            'q': self.city,
+            'appid': self.api_key,
+            'units': self.system
+        }
+        response = requests.get(self.base_url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Error encountered when calling the weather API")
+            return None
 
-    response = requests.get(FULL_URL)
-    if response.status_code == 200:
-        weather_data = response.json()
-        return weather_data
-    else:
-        print("Error encountered when calling the weather API")
+    def display_data(self, data_key, data_label, unit=""):
+        if self.weather_data:
+            data_value = self.weather_data['main'].get(data_key) if data_key in self.weather_data['main'] else self.weather_data['wind'].get(data_key) if data_key == 'speed' else self.weather_data['clouds'].get('all')
+            if data_value is not None:
+                ante = ""
+                if self.verbose:
+                    ante = f"{data_label} in {self.city} is: "
+                print(f"{ante}{data_value} {unit}")
+            else:
+                print(f"Error fetching {data_label.lower()} data")
+        else:
+            print("Error fetching data")
+
+    def display_temperature(self):
+        units = self.system_options.get(self.system, "")
+        self.display_data('temp', "Current temperature", units)
+
+    def display_wind(self):
+        self.display_data('speed', "Wind speed", "m/s")
+
+    def display_humidity(self):
+        self.display_data('humidity', "Humidity", "%")
+
+    def display_cloudiness(self):
+        self.display_data('all', "Cloudiness", "%")
 
 
-#functions to print weather's parameters
-def temp_funct(param):
-    
-    system_options = {"metric": "C", "imperial": "F", "standard": "K"}
-    final_data = get_weather(param)
-    if final_data:
-        current_temp = int(final_data['main']['temp'])
-        units = system_options.get(param['system'], "")
-
-        ante = ""
-        if param['verbose']:
-            ante = "Current temperature in {city} is: ".format(city=param['city']) 
-
-        print("{ante}{temp} {units}".format(ante=ante, temp=current_temp, units=units))
-    else:
-        print("Error fetching data")
-
-def wind_funct(param):
-    final_data = get_weather(param)
-    if final_data:
-        wind_speed = int(final_data['wind']['speed'])
-        if param['verbose'] == True:
-            ante = "Wind speed in {city} is: ".format(city=param['city'])
-        elif param['verbose'] == False:
-            ante = ""
-        print("{ante}{speed} m/s".format(ante=ante, speed=wind_speed))
-    else:
-        print("Error fetching data")
-
-def humidity_funct(param):
-    final_data = get_weather(param)
-    if final_data:
-        humidity = int(final_data['main']['humidity'])
-        if param['verbose'] == True:
-            ante = "Humidity in {city} is: ".format(city=param['city'])
-        elif param['verbose'] == False:
-            ante = ""
-        print("{ante}{humidity} %".format(ante=ante,humidity=humidity))
-    else:
-        print("Error fetching data")
-
-def clouds_funct(param):
-    final_data = get_weather(param)
-    if final_data:
-        clouds = int(final_data['clouds']['all'])
-        if param['verbose'] == True: 
-            ante = "Cloudiness in {city} is: ".format(city=param['city'])
-        elif param['verbose'] == False:
-            ante = ""
-        print("{ante}{clouds} %".format(ante=ante, clouds=clouds))
-    else:
-        print("Error fetching data")
-
-#main function to call all the functions above  
 def main():
     parser = argparse.ArgumentParser(description="Get weather information for a city.")
     parser.add_argument("-c", default="Bucharest", help="City name")
@@ -91,25 +65,18 @@ def main():
     parser.add_argument("-v", action="store_true", help="Verbose mode")
 
     args = parser.parse_args()
-    #make a dictionary with the parameters so that we can send only one object to functions:
-    param = {
-        'city': args.c,
-        'system': args.u,
-        'param': args.p,                    
-        'verbose': args.v
-    }
 
+    weather = Weather(city=args.c, system=args.u, verbose=args.v)
 
     if args.p == "t":
-        temp_funct(param)
+        weather.display_temperature()
     elif args.p == "w":
-        wind_funct(param)
+        weather.display_wind()
     elif args.p == "h":
-        humidity_funct(param)
+        weather.display_humidity()
     elif args.p == "c":
-        clouds_funct(param)
+        weather.display_cloudiness()
+
 
 if __name__ == "__main__":
     main()
-
-
